20b20303322ca80b0420b819cb1797a1bd3e1f3c
gkirion <gkirion@gmail.com>
2020-04-13 20:54:00 +0300

    initial commit

---
+++ src/main/java/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+
+    public static void hello(String name) {
+        System.out.println("hello " + name);
+    }
+
+    public static void goodbye(String name) {
+        System.out.println("goodbye " + name);
+    }
+
+}
